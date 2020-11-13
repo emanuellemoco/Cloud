b6a9997abb37262499add95fd68f1afa3a13a198
Emanuelle <emanuellesmoco@gmail.com>
2020-11-13 10:32:53 -0300

    funcoes basicas feitas

--- main.py
+++ main.py
@@ -5,14 +5,19 @@ import time
 ec2_e1 = boto3.client('ec2', region_name='us-east-1')
 ec2_e2 = boto3.client('ec2', region_name='us-east-2')
 
+elb_e1 = boto3.client('elbv2', region_name='us-east-1')
+elb_e2 = boto3.client('elbv2', region_name='us-east-2')
+
+
 
 ubuntu_AMI_name = 'ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-20200907'
 
+
+# FAZER UM WAITER PRO SECURITY GROUP
+
+
 #devemos usar o dry run?????
 
-
-# checar e matar todas as instancias e configuracoes 
-# com try e except caso não exista
 
 #Criar o django e sua imagem para o autoscalling
 
@@ -28,6 +33,20 @@ ubuntu_AMI_name = 'ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-2020090
 # procurar postgress
 # como criar usuario sem o prompt 
 #https://stackoverflow.com/questions/18715345/how-to-create-a-user-for-postgres-from-the-command-line-for-bash-automation
+
+
+def getSubnets(ec2):
+    subnets = []
+    sn_all = ec2.describe_subnets()
+    for sn in sn_all['Subnets'] :
+        subnets.append(sn['SubnetId'])
+    
+    return subnets
+        
+
+subnets_e1 = getSubnets(ec2_e1)
+subnets_e2 = getSubnets(ec2_e2)
+
 
 
 def createSecurityGroup(ec2, name):
@@ -47,27 +66,32 @@ def createSecurityGroup(ec2, name):
                 },
             ]
         )
-        print("Security Group {} criado".format(name))
+        print("Security Group {} created".format(name))
         return(security_group['GroupId'])
     except:
-        print("Deletando Security Group já existente")
+        print("Deletando sg que ja existe")
         response = ec2.delete_security_group(
         GroupName=name
         )
         return createSecurityGroup(ec2_e2, name)
 
-
-def getAMIid(ec2, name):
-
-    response = ec2.describe_images(
-        Filters=[
+def updateSecurityGroupRules(ec2, groupId):
+    response = ec2.authorize_security_group_ingress(
+        GroupId=groupId,
+        IpPermissions=[
             {
-                'Name': 'name',
-                'Values': [name]
+                'FromPort': 8080,
+                'IpProtocol': 'tcp',
+                'IpRanges': [
+                    {
+                        'CidrIp': '0.0.0.0/0',
+                    },
+                ],
+                'ToPort': 8080,
             },
-        ],
+        ]
     )
-    return response['Images'][0]['ImageId']
+    print("Security Group rules updated")
 
 
 
@@ -108,15 +132,11 @@ def createInstance(ami_id, ec2, instanceName, securityGroupID):
 
 
 def terminateInstances(ec2):
-    ### checar se o state é terminated
-
     response = ec2.describe_instances()
-    #print(response["Reservations"][0]["Instances"][0]["InstanceId"])
     for reservation in response["Reservations"]:
         #print(reservation["Instances"])
    
         for intance in reservation["Instances"]:
-            #print(intance["State"])
             if(intance["State"]["Name"] != "terminated"): 
                 for tags in intance["Tags"]:
                     if (tags["Key"] == "Creator"):
@@ -134,6 +154,7 @@ def createImage(ec2, instanceId, name):
             NoReboot=True
         )
         print("AMI created")
+        return response['ImageId']
     except:
         print("Unregistering existing AMI") 
         imageId= getAMIid(ec2_e1, name)
@@ -141,24 +162,153 @@ def createImage(ec2, instanceId, name):
         return createImage(ec2, instanceId, name)
 
 
+# Get AMI id by its name
+def getAMIid(ec2, name):
+    try:
+        response = ec2.describe_images(
+            Filters=[
+                {
+                    'Name': 'name',
+                    'Values': [name]
+                },
+            ],
+        )
+        return response['Images'][0]['ImageId']
+    except:
+        print("Imagem not found")
 
-def espera(ec2, instanceId):
+
+def getLoadBalancerArn(elb, name):
+    response = elb.describe_load_balancers()
+    for lb in response["LoadBalancers"]:
+        if (lb["LoadBalancerName"]==name):
+            LoadBalancerArn = lb["LoadBalancerArn"]
+            deleteLoadBalancer(elb, LoadBalancerArn)
+
+
+def createLoadBalancer(elb, name, port, subnets, securityGroupID):
+    #Check if there is a LB with the same name and delete it 
+    getLoadBalancerArn(elb_e1, name)
+
+    response = elb.create_load_balancer(
+        Name=name,
+        # SecurityGroups=[securityGroupID],
+        Tags=[
+            {
+                'Key': 'Creator',
+                'Value': 'manu'
+            },
+        ],
+        Subnets=subnets,
+    )
+    print("LoadBalancer {} created".format(name))
+    return(response["LoadBalancers"][0]["LoadBalancerArn"])
+
+
+def deleteLoadBalancer(elb, LoadBalancerArn):
+    print("Deleting existed LoadBalancer")
+    response = elb.delete_load_balancer(LoadBalancerArn=LoadBalancerArn)
+
+
+#criar e registrar target group
+
+def getVpcId(ec2):
+    response = ec2.describe_vpcs()
+    for vpc in response["Vpcs"]:
+        return vpc["VpcId"]
+    
+def getTargetGroupArn(elb, name):
+    response = elb.describe_target_groups()
+    print("___________")
+    print(response)
+    print("___________")
+    for tg in response["TargetGroups"]:
+        if (tg["TargetGroupName"]==name):
+            print("ja existe")
+            TargetGroupArn = tg["TargetGroupArn"]
+            deleteTargetGroup(elb, TargetGroupArn)
+
+def createTargetGroup(elb, port, name):
+    #Check if the TG already exists and delete it
+    getTargetGroupArn(elb, name)
+
+    print("Creating TargetGroup")
+    VpcId = getVpcId(ec2_e1)
+
+    response = elb.create_target_group(
+    Name=name,
+    Protocol='HTTP',
+    Port=port,
+    VpcId=VpcId,
+    TargetType='instance',
+    Tags=[
+        {
+            'Key': 'Creator',
+            'Value': 'manuuu'
+        },
+    ]
+)
+
+
+def deleteTargetGroup(elb, TargetGroupArn):
+    print("Deleting existed TargetGroup")
+    response = elb.delete_target_group(TargetGroupArn=TargetGroupArn)
+
+
+
+createTargetGroup(elb_e1, 8080, 'TargetGroupManu')
+
+
+
+
+def waiterInstance(ec2, instanceId):
     waiter = ec2.get_waiter('instance_running')
     waiter.wait(InstanceIds=[instanceId])
 
+def waiterSecurityGroup(ec2, groupId):
+    waiter = ec2.get_waiter('security_group_exists')
+    waiter.wait(GroupIds=[groupId])
+
+def waiterImage(ec2, imageId):
+    waiter = ec2.get_waiter('image_available')
+    waiter.wait(ImageIds=[imageId])
+
+
+# _________________________________TESTE INSTANCIAS________________________
+# # Terminate all instances
+# terminateInstances(ec2_e1)
+
+# # Getting ubuntu_20 AMI id 
+# AMI_ID_ubuntu_20 = (getAMIid(ec2_e1, ubuntu_AMI_name))
+
+# Creating a Security Group
+# securityGroupID = (createSecurityGroup(ec2_e1, 'securityGroupManu'))
+# waiterSecurityGroup(ec2_e1, securityGroupID)         
+# time.sleep(6)
+
+# # Creating an instance 
+# instanceId = createInstance(AMI_ID_ubuntu_20, ec2_e1, 'OLAR ', securityGroupID)
+# waiterInstance(ec2_e1, instanceId)
+
+# # Update the Security Group Rules
+# updateSecurityGroupRules(ec2_e1, securityGroupID)
+
+# # Creating image from an Instance
+# ImageIdTESTE = createImage(ec2_e1, instanceId, 'IMAGEMmanu' )
+
+# # time.sleep(10)
+# waiterImage(ec2_e1, ImageIdTESTE) 
+
+
+# #cria uam instancia com essa imagem
+# createInstance(ImageIdTESTE, ec2_e1, 'outra ', securityGroupID)
+
+# Creating load balancer
+# loadBalancerArn = createLoadBalancer(elb_e1, 'lbManu', 80, subnets_e1, 'a' ) 
+
+# _________________________________________________________________________
 
 
 
 
-terminateInstances(ec2_e1)
-
-AMI_ID_ubuntu_20 = (getAMIid(ec2_e1, ubuntu_AMI_name))
-
-securityGroupID = (createSecurityGroup(ec2_e1, 'securityGroupManu'))
-
-instanceId = createInstance(AMI_ID_ubuntu_20, ec2_e1, 'OLAR ', securityGroupID)
-
-espera(ec2_e1, instanceId)
-
-createImage(ec2_e1, instanceId, 'IMAGEMMMMMM' )
-
+print("FIM TESTE")
